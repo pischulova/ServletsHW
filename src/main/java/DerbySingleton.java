6786f0fd6b195d9f766a29d9ed3b54ae5b879be0
pischulova <pischulova@gmail.com>
2014-03-12 23:45:02 +0200

    Initial commit

--- src/main/java/DerbySingleton.java
+++ src/main/java/DerbySingleton.java
@@ -1,16 +1,25 @@
 import javax.persistence.*;
+import java.sql.*;
 
 /**
  * Created by Алена on 11.03.14.
  */
 public class DerbySingleton {
     private static DerbySingleton ourInstance = new DerbySingleton();
+    private static String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+//    private static final String DB_URL="jdbc:derby:C:\\Users\\Алена\\IdeaProjects\\ServletsHW\\db";
 
     private static final String UNIT_NAME = "Unit";
     private static EntityManagerFactory factory;
     private static EntityManager em = null;
 
+
     public static EntityManager getEMS() {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         if(em!=null) {
             return em;
         } else {
